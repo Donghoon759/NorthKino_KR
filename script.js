5d3d6cf72b9a3d36cfe985a14131c716284d6256
Donghoon Song <thdehdgns@gmail.com>
2017-12-16 17:26:07 +0200

    Adding modal and some changes

--- script.js
+++ script.js
@@ -47,3 +47,16 @@ $(document).ready(function(){
   
 
 });
+
+// modal
+
+$('.button').click(function(){
+  var buttonId = $(this).attr('id');
+  $('#modal-container').removeAttr('class').addClass(buttonId);
+  $('body').addClass('modal-active');
+})
+
+$('#modal-container').click(function(){
+  $(this).addClass('out');
+  $('body').removeClass('modal-active');
+});
